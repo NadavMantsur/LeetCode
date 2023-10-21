92ca108d4f935b91591b6a51561a7ea7a446f403
nadav.mantsur <nadav40450@gmail.com>
2023-10-21 14:29:37 +0300

    max depth binary tree in c

---
+++ c/#104_max_depth_binary_tree.c
@@ -0,0 +1,18 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+int maxDepth(struct TreeNode* root){
+    if (NULL == root){
+        return 0;
+    }
+
+    int maxLeft = maxDepth(root->left) + 1;
+    int maxRight = maxDepth(root->right) + 1;
+
+    return maxLeft > maxRight ? maxLeft : maxRight;
+}
